92caf78cbbaca3b358323a82881cc6842baeb55f
ILOVEMOZART <qkrrhkdxo123@naver.com>
2025-05-21 19:56:00 +0900

    Develop CSP

--- cfe/modules/srl/fsw/src/cfe_srl_csp.c
+++ cfe/modules/srl/fsw/src/cfe_srl_csp.c
@@ -12,12 +12,26 @@
 
 const char *SatName = "COSMIC"; // Revise name according to specific misison
 
+/**
+ * Indexed by CSP Node number.
+ */
+CFE_SRL_CSP_Node_Config_t *NodeConfig[32] = {0}; // Not mandatory
+
 int CFE_SRL_RouteInitCSP(void) {
     int Status;
 
     csp_iface_t *InterfaceCAN = NULL;
-    Status = csp_can_socketcan_open_and_add_interface(CSP_CAN_DEV_NAME, "CSPCAN0", 0, false, &InterfaceCAN);
-    if (Status < 0) return -1; // Revise to `ROUTE_INIT_ERR`
+
+    Status = csp_route_start_task(CSP_TASK_STACK_SIZE(1), GS_THREAD_PRIORITY_HIGH);
+    if (Status != CSP_ERR_NONE) {
+        CFE_ES_WriteToSysLog("%s: csp_route_start_task failed! CSP RC=%d\n", __func__, Status);
+        return Status;
+    }
+    InterfaceCAN = csp_can_socketcan_init(CSP_CAN_DEV_NAME, 0, false);
+    if (InterfaceCAN == NULL) {
+        CFE_ES_WriteToSysLog("%s: CSP Socket CAN Init failed! NO RC", __func__);
+        return -1; // Revise to `CSP_CAN_INIT_ERR`
+    }
 
     Status = csp_rtable_set(CSP_NODE_EPS, CSP_NODE_FIX_MASK, InterfaceCAN, CSP_NO_VIA_ADDRESS);
     if (Status != CSP_ERR_NONE) return -1; // Revise to `CSP_RTABLE_ERR`
@@ -34,6 +48,26 @@ int CFE_SRL_RouteInitCSP(void) {
     Status = csp_rtable_set(CSP_NODE_GSTRX, CSP_NODE_FIX_MASK, InterfaceCAN, CSP_NODE_UTRX);
     if (Status != CSP_ERR_NONE) return -1; // Revise to `CSP_RTABLE_ERR`
 
+    return CFE_SRL_OK;
+}
+
+int CFE_SRL_NodeConfigCSP(uint8_t Node, uint8_t Priority, uint32_t Timeout, uint32_t Options) {
+    if (NodeConfig[Node] == NULL) {
+        NodeConfig[Node] = (CFE_SRL_CSP_Node_Config_t *)malloc(sizeof(CFE_SRL_CSP_Node_Config_t));
+        memset(NodeConfig[Node], 0, sizeof(CFE_SRL_CSP_Node_Config_t));
+    }
+
+    NodeConfig[Node]->Priority = Priority;
+    NodeConfig[Node]->Timeout = Timeout;
+    NodeConfig[Node]->Options = Options;
+
+    return CFE_SRL_OK;
+}
+
+int CFE_SRL_GetNodeConfigCSP(uint8_t Node, CFE_SRL_CSP_Node_Config_t *Config) {
+    if (NodeConfig[Node] == NULL) return CFE_SRL_NOT_FOUND;
+
+    Config = NodeConfig[Node];
     return CFE_SRL_OK;
 }
 
@@ -60,12 +94,38 @@ int CFE_SRL_InitCSP(void) {
         return Status;
     }
 
-    Status = csp_route_start_task(1024, GS_THREAD_PRIORITY_HIGH);
-    if (Status != CSP_ERR_NONE) {
-        CFE_ES_WriteToSysLog("%s: csp_route_start_task failed! CSP RC=%d\n", __func__, Status);
-        return Status;
+    /**
+     * Init CSP CAN and Register Routing Configuration
+     */
+    Status = CFE_SRL_RouteInitCSP();
+    if (Status != CFE_SRL_OK) {
+        CFE_ES_WriteToSysLog("%s: CSP Route Init failed! CSP RC=%d\n", __func__, Status);
+        return -1; // Revise to `ROUTE_INIT_ERR`
     }
 
-    Status = CFE_SRL_RouteInitCSP();
 
+    /**
+     * Register Node Configuration to each Node.
+     * If Node appended(or revised), insert(or revise) the function.
+     */
+    CFE_SRL_NodeConfigCSP(CSP_NODE_EPS, CSP_PRIO_NORM, CSP_TIMEOUT(1), CSP_O_CRC32);
+    CFE_SRL_NodeConfigCSP(CSP_NODE_UTRX, CSP_PRIO_NORM, CSP_TIMEOUT(1), CSP_O_CRC32);
+    CFE_SRL_NodeConfigCSP(CSP_NODE_STRX, CSP_PRIO_NORM, CSP_TIMEOUT(1), CSP_O_CRC32);
+
+    CFE_SRL_NodeConfigCSP(CSP_NODE_GS_KISS, CSP_PRIO_HIGH, CSP_TIMEOUT(3), CSP_O_CRC32);
+    CFE_SRL_NodeConfigCSP(CSP_NODE_GSTRX, CSP_PRIO_HIGH, CSP_TIMEOUT(3), CSP_O_CRC32);
+
+    return CFE_SRL_OK;
+}
+
+int CFE_SRL_TransactionCSP(uint8_t Node, uint8_t Port, const void *TxData, int TxSize, void *RxData, int RxSize) {
+    // If there is no reply, Put `RxSize = 0`
+    int Status;
+    if (NodeConfig[Node] == NULL || TxData == NULL || RxData == NULL) return CFE_SRL_NULL_ERR;
+
+    Status = csp_transaction_w_opts(NodeConfig[Node]->Priority, Node, Port, NodeConfig[Node]->Timeout, TxData, TxSize, RxData, RxSize, NodeConfig[Node]);
+    if (Status == 0) return -1; // Revise to `CSP_TRANSACTION_ERR`
+
+    // 1 or `reply size` on success
+    return Status;
 }
--- cfe/modules/srl/fsw/src/cfe_srl_csp.h
+++ cfe/modules/srl/fsw/src/cfe_srl_csp.h
@@ -5,16 +5,34 @@
 
 #include "cfe_srl_basic.h"
 
-#define CSP_CAN_DEV_NAME     "can0"
+#define CSP_TASK_STACK_SIZE(x)      (x)*1024
 
-#define CSP_NODE_FIX_MASK        5
+#define CSP_CAN_DEV_NAME            "can0"
+
+#define CSP_NODE_FIX_MASK           5
+
+#define CSP_TIMEOUT(x)              (x)*1000
+
+/**
+ * COSMIC CSP Nodes
+ * 
+ * SANT is GomSpace but, doesn't need CSP
+ * Consider CubeSpace's ADCS Solution.
+ */
 typedef enum {
     CSP_NODE_EPS = 2,   // EPS Dock p31u Node
     CSP_NODE_OBC = 3,
     CSP_NODE_UTRX = 5,
     CSP_NODE_STRX = 6,
     CSP_NODE_GS_KISS = 8,
-    CSP_NODE_GSTRX = 20
-    
+    CSP_NODE_GSTRX = 20,
+    // CSP_NODE_ADCS = 24
 } CFE_SRL_CSP_Node_t;
+
+typedef struct {
+    uint8_t Priority;
+    uint32_t Timeout;
+    uint32_t Options;
+} CFE_SRL_CSP_Node_Config_t;
+
 #endif /* CFE_SRL_CSP_H */
